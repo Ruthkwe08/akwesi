5402415b6d013b4d97e162799fb89f5863cda2ff
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-15 20:06:55 -0500

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -8,25 +8,25 @@ locals{
     ]
 ])
 }
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "examples" {
   name     = "LoadBalancerRG"
   location = "West Europe"
 }
 
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "examples" {
   name                = "PublicIPForLB"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.examples.location
+  resource_group_name = azurerm_resource_group.examples.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_lb" "example" {
+resource "azurerm_lb" "examples" {
   name                = "TestLoadBalancer"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.examples.location
+  resource_group_name = azurerm_resource_group.examples.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    public_ip_address_id = azurerm_public_ip.examples.id
   }
 }
