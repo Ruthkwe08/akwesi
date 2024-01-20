7bdac08591699a45a41e52986c654c227af191cc
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-19 19:08:36 -0500

    Update linux_web_app.tf

--- linux_web_app.tf
+++ linux_web_app.tf
@@ -2,20 +2,20 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example.ruth" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
+resource "azurerm_service_plan" "example.ruth" {
+  name                = "example.ruth"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "example.ruth" {
   name                = "example"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
